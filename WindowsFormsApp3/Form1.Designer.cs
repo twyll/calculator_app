399f349c3bba076b627aa15ec5af163f0c311609
twyll <39091035+twyll@users.noreply.github.com>
2018-07-09 13:03:18 +0600

    added switch
    added switch

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.addition = new System.Windows.Forms.Button();
+            this.substraction = new System.Windows.Forms.Button();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,73 +42,81 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 37);
+            this.textBox1.Location = new System.Drawing.Point(32, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.Size = new System.Drawing.Size(99, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(156, 37);
+            this.textBox2.Location = new System.Drawing.Point(208, 46);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.Size = new System.Drawing.Size(99, 22);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(43, 182);
+            this.textBox3.Location = new System.Drawing.Point(57, 224);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(171, 20);
+            this.textBox3.Size = new System.Drawing.Size(227, 22);
             this.textBox3.TabIndex = 2;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // button1
+            // addition
             // 
-            this.button1.Location = new System.Drawing.Point(24, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.addition.Location = new System.Drawing.Point(32, 95);
+            this.addition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(100, 28);
+            this.addition.TabIndex = 3;
+            this.addition.Text = "+";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.buttonClick);
             // 
-            // button2
+            // substraction
             // 
-            this.button2.Location = new System.Drawing.Point(24, 118);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "-";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.substraction.Location = new System.Drawing.Point(32, 145);
+            this.substraction.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.substraction.Name = "substraction";
+            this.substraction.Size = new System.Drawing.Size(100, 28);
+            this.substraction.TabIndex = 4;
+            this.substraction.Text = "-";
+            this.substraction.UseVisualStyleBackColor = true;
+            this.substraction.Click += new System.EventHandler(this.buttonClick);
             // 
-            // button3
+            // multiplication
             // 
-            this.button3.Location = new System.Drawing.Point(156, 77);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "*";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.multiplication.Location = new System.Drawing.Point(208, 95);
+            this.multiplication.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(100, 28);
+            this.multiplication.TabIndex = 5;
+            this.multiplication.Text = "*";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.buttonClick);
             // 
-            // button4
+            // division
             // 
-            this.button4.Location = new System.Drawing.Point(156, 118);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "/";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.division.Location = new System.Drawing.Point(208, 145);
+            this.division.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(100, 28);
+            this.division.TabIndex = 6;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.buttonClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 18);
+            this.label1.Location = new System.Drawing.Point(53, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.Size = new System.Drawing.Size(62, 17);
             this.label1.TabIndex = 7;
             this.label1.Text = "Число A";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -116,9 +124,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(165, 18);
+            this.label2.Location = new System.Drawing.Point(220, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.Size = new System.Drawing.Size(62, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Число B";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -126,27 +135,29 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(99, 155);
+            this.label3.Location = new System.Drawing.Point(132, 191);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.Size = new System.Drawing.Size(76, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "Результат";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 227);
+            this.ClientSize = new System.Drawing.Size(345, 279);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiplication);
+            this.Controls.Add(this.substraction);
+            this.Controls.Add(this.addition);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Калькулятор";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -160,17 +171,17 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.Button substraction;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.Button division;
 
 
-        public static double sum(double a, double b)
+        public static double add(double a, double b)
         {
             return a + b;
         }
-        public static double diff(double a, double b)
+        public static double substr(double a, double b)
         {
             return a - b;
         }
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp3
@@ -26,35 +19,27 @@ namespace WindowsFormsApp3
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonClick(object sender, EventArgs e)
         {
             double firstArgument = Convert.ToDouble(textBox1.Text);
             double secondArgument = Convert.ToDouble(textBox2.Text);
-            textBox3.Text = Convert.ToString(sum(firstArgument, secondArgument));
-        }
+            switch (((Button)sender).Name)
+            {
+                case "addition":
+                    textBox3.Text = Convert.ToString(add(firstArgument, secondArgument));
+                    break;
+                case "substraction":
+                    textBox3.Text = Convert.ToString(substr(firstArgument, secondArgument));
+                    break;
+                case "multiplication":
+                    textBox3.Text = Convert.ToString(multiply(firstArgument, secondArgument));
+                    break;
+                case "division":
+                    textBox3.Text = Convert.ToString(div(firstArgument, secondArgument));
+                    break;
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            double firstArgument = Convert.ToDouble(textBox1.Text);
-            double secondArgument = Convert.ToDouble(textBox2.Text);
-            textBox3.Text = Convert.ToString(multiply(firstArgument, secondArgument));
+            }
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            double firstArgument = Convert.ToDouble(textBox1.Text);
-            double secondArgument = Convert.ToDouble(textBox2.Text);
-            textBox3.Text = Convert.ToString(diff(firstArgument, secondArgument));
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            double firstArgument = Convert.ToDouble(textBox1.Text);
-            double secondArgument = Convert.ToDouble(textBox2.Text);
-            textBox3.Text = Convert.ToString(div(firstArgument, secondArgument));
-        }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
