acf8ead94fbd6f91406f569b6f3af3890265a2fa
vikasycheva <40975570+vikasycheva@users.noreply.github.com>
2018-07-10 13:22:56 +0600

    check for 0

--- WindowsFormsApp3/TwoArgumentOperation/DivisionCalculator.cs
+++ WindowsFormsApp3/TwoArgumentOperation/DivisionCalculator.cs
@@ -1,9 +1,15 @@
-﻿namespace WindowsFormsApp3.TwoArgumentOperation
+﻿using System;
+
+namespace WindowsFormsApp3.TwoArgumentOperation
 {
     public class DivisionCalculator : ITwoArgumentCalculator
     {
         public double Calculate(double firstValue, double secondValue)
         {
+            if (secondValue == 0)
+            {
+                throw new Exception("Деление на 0");
+            }
             return firstValue / secondValue;
         }
     }
