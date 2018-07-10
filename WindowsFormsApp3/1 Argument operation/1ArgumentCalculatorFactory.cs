17e54c80450925e6d091178b999aa3ce88d180ef
vikasycheva <40975570+vikasycheva@users.noreply.github.com>
2018-07-10 10:45:21 +0600

    added factory for 1 argument calculator
    added factory for 1 argument calculator

---
+++ WindowsFormsApp3/1 Argument operation/1ArgumentCalculatorFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3._1_Argument_operation
+{
+    class _1ArgumentCalculatorFactory
+    {
+        public static I1ArgumentCalculator createCalculator(string operationName)
+        {
+            switch (operationName)
+            {
+                case "sin":
+                    return new SineCalculator();
+                case "cos":
+                    return new CosCalculator();
+                default:
+                    throw new ArgumentException("Неизвестная операция", "operationName");
+            }
+        }
+    }
+}
